795ea68efabe03e72a1a063f403b4bf6b83864e7
StratiX0 <jerome.hussenet@sfr.fr>
2024-12-06 00:07:46 +0100

    feat: Moving Directional Light

--- Assets/Scripts/MainLight.cs
+++ Assets/Scripts/MainLight.cs
@@ -4,13 +4,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class GetMainLightDirection : MonoBehaviour
+public class MainLight : MonoBehaviour
 {
     
     [SerializeField] private Material skyboxMaterial;
+    private float angle = 0;
+    [SerializeField] private float speed;
 
     private void Update()
     {
+        angle += Time.deltaTime * speed;
+        transform.localRotation = Quaternion.Euler(angle, 0, 0);
         skyboxMaterial.SetVector("_MainLightDirection", transform.forward);
     }
 }
